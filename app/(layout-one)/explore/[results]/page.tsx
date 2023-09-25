87792db7476861cc665f7d62df697b68e14764ff
Abhishek Mamgain <abhishekmmgn@gmail.com>
2023-09-25 22:29:29 +0530

    Created explore page and components

---
+++ app/(layout-one)/explore/[results]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+6
+export default function SearchResultsPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ app/(layout-one)/explore/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import Navbar from "@/components/navbar";
+import SearchBar from "@/components/searchbar";
+import TableRow from "@/components/table-row";
+import SearchResult from "./search-result";
+
+export default function SearchPage() {
+  const [isActive, setActive] = useState(false);
+
+  function handleSearchBarToggle() {
+    setActive(!isActive);
+  }
+  function toggleSearchBarOn() {
+    setActive(true);
+  }
+  return (
+    <>
+      <Navbar title="Search" />
+      <>
+        <SearchBar
+          active={isActive}
+          placeholder="Search organizations"
+          handleClick={handleSearchBarToggle}
+          handleFocus={toggleSearchBarOn}
+        />
+        {isActive ? (
+          <div className="w-full h-full py-2 transition-all ease-in-out duration-300">
+            <SearchResult />
+            <SearchResult />
+          </div>
+        ) : (
+          <>
+            <h2 className="pt-3 pb-1 px-5 text-2xl font-medium">
+              Search by Cause
+            </h2>
+            <>
+              <TableRow title="Environment" />
+              <TableRow title="Social Work" />
+              <TableRow title="Education" />
+              <TableRow title="Animals" />
+              <TableRow title="Health" />
+              <TableRow title="Cancer" />
+              <TableRow title="Arts" />
+              <TableRow title="Human Rights" />
+              <TableRow title="International" />
+              <TableRow title="Religion" />
+              <TableRow title="Community" />
+              <TableRow title="Disaster Relief" />
+              <TableRow title="Sports" />
+              <TableRow title="Other" />
+            </>
+          </>
+        )}
+      </>
+    </>
+  );
+}
