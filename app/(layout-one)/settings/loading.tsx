7253c6d77315a50c152cfc44e3449c8361e0d39e
Abhishek Mamgain <abhishekmmgn@gmail.com>
2023-09-28 22:06:04 +0530

    Completed settings part

---
+++ app/(layout-one)/settings/loading.tsx
@@ -0,0 +1,16 @@
+import LoadingSkeleton from "@/components/loading-skeleton"
+
+export default function Loading() {
+  return (
+    <div className="p-5 space-y-4 ">
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+      <LoadingSkeleton />
+    </div>
+  )
+}
--- app/(layout-one)/settings/page.tsx
+++ app/(layout-one)/settings/page.tsx
@@ -1,27 +1,33 @@
 import Navbar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import NotificationsPrefrences from "@/components/settings/notifications-prefrences";
-import ChangePassword from "@/components/settings/change-password";
+import NotificationsPrefrences from "./notifications-prefrences";
+import ChangePassword from "@/app/(layout-one)/settings/change-password";
+import ThemeToggle from "@/app/(layout-one)/settings/theme-toggler";
 
 export default function SettingsPage() {
   return (
     <>
       <Navbar title="Settings" />
-      <div className="p-4 px-5 space-y-5 ">
+      <div className="p-4 px-5 space-y-5 lg:px-0">
+        <div className="flex justify-between items-center">
+          <h3 className="mb-1 text-lg font-medium">Theme</h3>
+          <ThemeToggle />
+        </div>
+        <Separator />
         <NotificationsPrefrences />
         <Separator />
         <ChangePassword />
         <Separator />
         <div>
           <h3 className="mb-1 text-lg font-medium">Sign Out</h3>
-          <p className="mb-3 text-sm text-[#464646]">
+          <p className="mb-3 text-sm text-muted-foreground">
             Sign out from current session.
           </p>
           <Button variant="secondary" className="mx-auto mb-5">
             Sign Out of This Devices
           </Button>
-          <p className="mb-3 text-sm text-[#464646]">
+          <p className="mb-3 text-sm text-muted-foreground">
             Sign out of all devices and browsers. You will need to sign in again
           </p>
           <Button variant="secondary" className="mx-auto">
@@ -31,7 +37,7 @@ export default function SettingsPage() {
         <Separator />
         <div>
           <h3 className="mb-1 text-lg font-medium">Delete Account</h3>
-          <p className="mb-4 text-sm text-[#464646]">
+          <p className="mb-4 text-sm text-muted-foreground">
             Deleting your account will: Delete your profile, along with your
             authentication associations. Delete any and all content you have,
             such as posts, comments, or your messages.
