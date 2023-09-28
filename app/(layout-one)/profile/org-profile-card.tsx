d03c0ed24dfc6a9ed5ea52f27b296e7fca54df8c
Abhishek Mamgain <abhishekmmgn@gmail.com>
2023-09-28 22:05:51 +0530

    Completed profile part

---
+++ app/(layout-one)/profile/org-profile-card.tsx
@@ -0,0 +1,36 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "../../../components/ui/button";
+import Link from "next/link";
+
+type propsType = {
+  last?: boolean;
+  uid?: string;
+};
+
+export default function OrgProfileCard(props: propsType) {
+  return (
+    <div className="py-2  dark: hover:bg-muted hover:dark:bg-muted">
+      <div className="w-full h-[72px] flex items-center px-5 py-2 gap-4 lg:px-0">
+        <Avatar className="w-14 h-14">
+          <AvatarImage src="https://www.unsplash.com/random" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="w-full flex flex-col justify-center items-center">
+          <h1 className="w-full overflow-x-hidden line-clamp-1 text-lg font-medium">
+            DKMS UK
+          </h1>
+          <p className="w-full overflow-x-hidden text-sm line-clamp-1 text-[#3c3c3e] dark:text-[#a1a1a1]">
+            We Delete Blood Cancer
+          </p>
+        </div>
+      </div>
+      <div className="px-5 w-full lg:px-0">
+        <Button variant="secondary" asChild>
+          <Link href="/login">Edit Profile</Link>
+        </Button>
+      </div>
+      {!props.last && <Separator />}
+    </div>
+  );
+}
--- app/(layout-one)/profile/page.tsx
+++ app/(layout-one)/profile/page.tsx
@@ -1,60 +1,72 @@
 import Link from "next/link";
 import Navbar from "@/components/navbar";
-import OrgProfileCard from "../../../components/profile/org-profile-card";
-import ProfileCard from "@/components/profile/profile-card";
+import OrgProfileCard from "./org-profile-card";
+import ProfileCard from "./profile-card";
 import { Separator } from "@/components/ui/separator";
-import Post from "@/components/posts/post";
-import EventPost from "@/components/posts/event-post";
+import Post from "../post/post";
+import EventPost from "../post/event-post";
 import TableRow from "@/components/table-row";
 
 export default function ProfilePage() {
-  const isUser = false;
+  const isUser = true;
   return (
     <>
       <Navbar title="Profile" />
-      <div className="pb-4 z">
+      <div className="pb-4">
         {isUser ? <ProfileCard last={true} /> : <OrgProfileCard last={true} />}
         <Separator />
-        <div className="w-full h-16 flex items-center px-5 gap-4  hover:bg-muted">
+        <div className="w-full h-16 flex items-center px-5 gap-4  hover:bg-muted lg:px-0">
           <p className="font-medium">Organizations Following</p>
           <p className="text-primary">12</p>
         </div>
         <Separator />
         {!isUser && (
           <>
-            <div className="w-full h-16 flex items-center px-5 gap-4  hover:bg-muted ">
+            <div className="w-full h-16 flex items-center px-5 gap-4  hover:bg-muted lg:px-0">
               <p className="font-medium">Followers</p>
               <p className="text-primary">12K</p>
             </div>
             <Separator />
           </>
         )}
-        <div className="pt-6 px-5 space-y-4">
-          <div className="space-y-2">
+        {isUser ? (
+          <div className="pt-6 px-5 lg:px-0 space-y-2">
             <div className="flex justify-between items-center">
-              <h1 className="font-medium text-2xl">Posts</h1>
+              <h2 className="font-medium text-2xl">Activity</h2>
               <Link href="/explore">
                 <p className="text-primary text-sm">See All</p>
               </Link>
             </div>
-            <div className="">
-              <Post paddingX={true} />
-              <Separator />
+            <Post paddingX={true} />
+          </div>
+        ) : (
+          <div className="pt-6 px-5 space-y-4 lg:px-0">
+            <div className="space-y-2">
+              <div className="flex justify-between items-center">
+                <h1 className="font-medium text-2xl">Posts</h1>
+                <Link href="/explore">
+                  <p className="text-primary text-sm">See All</p>
+                </Link>
+              </div>
+              <div>
+                <Post paddingX={true} />
+                <Separator />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <div className="flex justify-between items-center">
+                <h1 className="font-medium text-2xl">Events</h1>
+                <Link href="/explore">
+                  <p className="text-primary text-sm">See All</p>
+                </Link>
+              </div>
+              <div>
+                <EventPost paddingX={true} />
+                <Separator />
+              </div>
             </div>
           </div>
-          <div className="space-y-2">
-            <div className="flex justify-between items-center">
-              <h1 className="font-medium text-2xl">Events</h1>
-              <Link href="/explore">
-                <p className="text-primary text-sm">See All</p>
-              </Link>
-            </div>
-            <div className="">
-              <EventPost paddingX={true} />
-              <Separator />
-            </div>
-          </div>
-        </div>
+        )}
         <Link href="/settings">
           <TableRow title="Settings" textColor={false} />
         </Link>
