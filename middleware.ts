e2a76fbfdf0ac1c8da847c7c6ffe34f815be69bc
Abhishek Mamgain <abhishekmmgn@gmail.com>
2024-05-29 22:49:17 +0530

    Just a deployment

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-// import { cookies } from "next/headers";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
